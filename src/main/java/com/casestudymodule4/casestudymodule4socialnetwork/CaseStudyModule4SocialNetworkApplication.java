a3d006c3399f1f7006f878e4e9652796af45e258
ishopjapan <thoitran2107@gmail.com>
2022-10-03 09:40:43 +0700

    tao du an

---
+++ src/main/java/com/casestudymodule4/casestudymodule4socialnetwork/CaseStudyModule4SocialNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.casestudymodule4.casestudymodule4socialnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseStudyModule4SocialNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaseStudyModule4SocialNetworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/casestudymodule4/casestudymodule4socialnetwork/CaseStudyModule4SocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casestudymodule4.casestudymodule4socialnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseStudyModule4SocialNetworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
